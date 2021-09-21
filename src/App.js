02430e4486239174da5e04dd01189802cb77b2cd
mbarone1 <mbarone@activar.app>
2021-09-20 22:03:50 -0300

    navbar - problemas con npm start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, NavDropdown, Container, Nav} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Mi Ecommerce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+          <Nav.Link href="#Login">Iniciar Sesion</Nav.Link>
+          <Nav.Link eventKey={2} href="#Registro">
+            Registrarse
+          </Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  )
 }
-
 export default App;
