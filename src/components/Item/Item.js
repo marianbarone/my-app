0e7a133bcc62ae6df17a193d3754c60377264ee3
mbarone1 <mbarone@activar.app>
2021-09-30 20:09:45 -0300

    Intento de map. Falla al importarlo en item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,17 +1,20 @@
 import './Item.css';
 import ItemCount from '../ItemCount/ItemCount';
 import TheDarkKnightRises from './TheDarkKnightRises.jpg'
+// import Movies from '../Movies/Movies'
+import moviesTitle from '../Movies/Movies'
+
 
 const Item = () => {
   return (
     <>
       <div className="card">
         <img className="imgProducto" src={TheDarkKnightRises} alt="The Dark Knight Rises"></img>
-        <h4 className="nombreProducto">The Dark Knight Rises</h4>
+        <h4 className="nombreProducto">{moviesTitle}</h4>
         <h6 className="year">YEAR: 2012</h6>
         <h6 className="IMDB">8.4 IMDB | 8.6 Rotten Tomatoes</h6>
         <h6 className="cantidad">Cantidad</h6>
-        <ItemCount className="rightAlign"/>
+        <ItemCount />
         <h5 className="precioProducto">$ 20</h5>
       </div>
     </>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,8 +1,23 @@
-let movies = [
-    {id: 0, title:"Batman Begins",poster:"url", year:"2012", rating:"8.4", precio: 950},
-    {id: 1, title:"The Dark Knight",poster:"url", year:"2012", rating:"8.4", precio:1450},
-    {id: 2, title:"The Dark Knight Rises",poster:"url", year:"2012", rating:"8.4", precio:400},
-    {id: 3, title:"Matrix", poster:"url", year:"2012", rating:"8.4", precio:420},
-    {id: 4, title:"Matrix Reloaded",poster:"url", year:"2012", rating:"8.4", precio:750},
-    {id: 4, title:"Matrix Revolutions",poster:"url", year:"2012", rating:"8.4", precio:750},
-];
+import React from 'react'
+
+function Movies () {
+    const data = [
+        {id: 0, title:"Batman Begins",poster:"url", year:"2012", rating:"8.4", precio: 950},
+        {id: 1, title:"The Dark Knight",poster:"url", year:"2012", rating:"8.4", precio:1450},
+        {id: 2, title:"The Dark Knight Rises",poster:"url", year:"2012", rating:"8.4", precio:400},
+        {id: 3, title:"Matrix", poster:"url", year:"2012", rating:"8.4", precio:420},
+        {id: 4, title:"Matrix Reloaded",poster:"url", year:"2012", rating:"8.4", precio:750},
+        {id: 4, title:"Matrix Revolutions",poster:"url", year:"2012", rating:"8.4", precio:750},
+    ];
+
+    const moviesTitle = data.map(movie => <Movies movie={movie.title}/>)
+    console.log(moviesTitle);
+  
+    return (
+        <div>
+            {moviesTitle}
+        </div>
+    );
+}
+
+export default Movies;
